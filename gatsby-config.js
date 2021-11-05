aa40dbbbc17e035119fe501912a3a554a02ff46e
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-11-05 12:44:46 +0900

    :+1: Update : fabicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `./static/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `./static/icon.jpeg`, // This path is relative to the root of the site.
       },
     },
     // `gatsby-plugin-offline`,
